96aff224b5d9e2200134d9c5daf89f08d6ed23a2
Robert Matundu <matundukabamba@gmail.com>
2024-06-30 18:18:16 +0200

    last commit

--- react/src/views/Dashboard.jsx
+++ react/src/views/Dashboard.jsx
@@ -2,6 +2,8 @@
 
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <>
+      <h2 className="text-3xl text-red-900 font-extrabold">Tableau de Bord</h2>
+    </>
   )
 }
