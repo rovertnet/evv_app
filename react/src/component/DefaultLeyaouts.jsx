69a6b7e0648ff14d9acf2f1024ee6949eca8c7ad
Robert Matundu <matundukabamba@gmail.com>
2024-06-30 20:08:01 +0200

    last commit

--- react/src/component/DefaultLeyaouts.jsx
+++ react/src/component/DefaultLeyaouts.jsx
@@ -55,7 +55,6 @@ export default function DefaultLeyaouts() {
                                 : "text-gray-300 hover:bg-gray-700 hover:text-white",
                               "rounded-md px-3 py-2 text-sm font-medium"
                             )}
-                            aria-current={item.current ? "page" : undefined}
                           >
                             {item.name}
                           </NavLink>
@@ -134,20 +133,18 @@ export default function DefaultLeyaouts() {
               <DisclosurePanel className="md:hidden">
                 <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
                   {navigation.map((item) => (
-                    <DisclosureButton
-                      key={item.name}
-                      as="a"
-                      href={item.href}
-                      className={classNames(
-                        item.current
+                    <NavLink
+                      key={item.nto}
+                      to={item.to}
+                      className={({isActive}) =>classNames(
+                        isActive
                           ? "bg-gray-900 text-white"
                           : "text-gray-300 hover:bg-gray-700 hover:text-white",
                         "block rounded-md px-3 py-2 text-base font-medium"
                       )}
-                      aria-current={item.current ? "page" : undefined}
                     >
                       {item.name}
-                    </DisclosureButton>
+                    </NavLink>
                   ))}
                 </div>
                 <div className="border-t border-gray-700 pb-3 pt-4">
