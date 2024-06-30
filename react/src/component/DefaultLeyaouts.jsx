24022d39cbc3962a1f43215a6d037d64cd730d89
Robert Matundu <matundukabamba@gmail.com>
2024-06-30 20:01:15 +0200

    last commit

--- react/src/component/DefaultLeyaouts.jsx
+++ react/src/component/DefaultLeyaouts.jsx
@@ -14,7 +14,7 @@ const user = {
   name: "Tom Cook",
   email: "tom@example.com",
   imageUrl:
-    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    "../../public/robert.png",
 };
 const navigation = [
   { name: "Dashboard", to: "/"},
@@ -49,8 +49,8 @@ export default function DefaultLeyaouts() {
                           <NavLink
                             key={item.name}
                             to={item.to}
-                            className={classNames(
-                              item.current
+                            className={({isActive}) => classNames(
+                              isActive
                                 ? "bg-gray-900 text-white"
                                 : "text-gray-300 hover:bg-gray-700 hover:text-white",
                               "rounded-md px-3 py-2 text-sm font-medium"
