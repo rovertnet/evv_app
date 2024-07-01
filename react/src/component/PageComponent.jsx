268f45f715c95fb2899efedb5dfc851f9997f297
Robert Matundu <matundukabamba@gmail.com>
2024-07-01 19:01:50 +0200

    je suis le deuxième commit

--- react/src/component/PageComponent.jsx
+++ react/src/component/PageComponent.jsx
@@ -4,10 +4,11 @@ export default function PageComponent({ title, buttons="", children }) {
   return (
     <>
       <header className="bg-gray-800 shadow">
-        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <div className="flex justify-between items-center mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold tracking-tight text-slate-100">
             {title}
           </h1>
+          {buttons}
         </div>
       </header>
       <main>
