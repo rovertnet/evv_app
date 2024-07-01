189425cf3b357169b41f4ea4eea87ad30b26f72b
Robert Matundu <matundukabamba@gmail.com>
2024-07-01 19:05:15 +0200

    je suis le deuxième commit

--- react/src/views/Surveys.jsx
+++ react/src/views/Surveys.jsx
@@ -1,20 +1,12 @@
+import PageComponent from "../component/PageComponent";
 
 
 export default function Surveys() {
   return (
     <>
-      <header className="bg-gray-800 shadow">
-        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold tracking-tight text-slate-100">
-            Surveys
-          </h1>
-        </div>
-      </header>
-      <main>
-        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          {/* Your content */}
-        </div>
-      </main>
+      <PageComponent title={"Surveys"}>
+        children
+      </PageComponent>
     </>
   );
 }
