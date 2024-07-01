0e6d98529c6cb2e1db588c0a19440144ec896c08
Robert Matundu <matundukabamba@gmail.com>
2024-07-01 17:30:07 +0200

    je suis le deuxième commit

--- react/src/views/Dashboard.jsx
+++ react/src/views/Dashboard.jsx
@@ -3,9 +3,9 @@
 export default function Dashboard() {
   return (
     <>
-      <header className="bg-white shadow">
+      <header className="bg-gray-800 shadow">
         <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+          <h1 className="text-3xl font-bold tracking-tight text-slate-100">
             Tableau de Bord
           </h1>
         </div>
