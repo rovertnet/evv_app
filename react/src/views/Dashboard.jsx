eacf97661e7d9e14a2493eb8ce5735c1d08e8ab8
Robert Matundu <matundukabamba@gmail.com>
2024-07-01 19:04:04 +0200

    je suis le deuxième commit

--- react/src/views/Dashboard.jsx
+++ react/src/views/Dashboard.jsx
@@ -1,20 +1,12 @@
+import PageComponent from "../component/PageComponent";
 
 
 export default function Dashboard() {
   return (
     <>
-      <header className="bg-gray-800 shadow">
-        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold tracking-tight text-slate-100">
-            Tableau de Bord
-          </h1>
-        </div>
-      </header>
-      <main>
-        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          Content
-        </div>
-      </main>
+      <PageComponent title={"Dashboard"}>
+        children
+      </PageComponent>
     </>
   );
 }
