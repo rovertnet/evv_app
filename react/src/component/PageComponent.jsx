fbb4adfaeb75b09b3b2f9d1006e7857bdaea33ae
Robert Matundu <matundukabamba@gmail.com>
2024-07-01 18:57:54 +0200

    je suis le deuxième commit

--- react/src/component/PageComponent.jsx
+++ react/src/component/PageComponent.jsx
@@ -1,18 +1,18 @@
 
 
-export default function Dashboard() {
+export default function PageComponent({ title, buttons="", children }) {
   return (
     <>
       <header className="bg-gray-800 shadow">
         <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold tracking-tight text-slate-100">
-            Tableau de Bord
+            {title}
           </h1>
         </div>
       </header>
       <main>
         <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          Content
+          {children}
         </div>
       </main>
     </>
